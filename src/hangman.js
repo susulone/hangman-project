d7dedab301c1af73e910d5a81c78dc2673bbe020
Suvi.Sulonen <suvi.sulonen@tuni.fi>
2022-04-12 20:15:10 +0300

    Add: (hangman.js) to include license and copyright statement.

--- src/hangman.js
+++ src/hangman.js
@@ -1,6 +1,23 @@
 // @ts-check
 
 /**
+ * @license
+ * @copyright 2022
+ * @author Suvi Sulonen <suvi.sulonen@gmail.com>
+ *
+ * This file is part of Hangman Test Project.
+ *
+ * Hangman Test Project is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hangman Test Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Hangman Test Project. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * This module contains the ASCII art for the different stages of the gallows and a method for returning the correct stage.
  * @module hangman
  *@namespace hangman
@@ -13,67 +30,67 @@ const hangman = {
    */
   stages: [
     `
-        +---+
-        |   |
-            |
-            |
-            |
-            |
-      =========
+      +---+
+      |   |
+          |
+          |
+          |
+          |
+    =========
       `,
     `
-        +---+
-        |   |
-        O   |
-            |
-            |
-            |
-      =========
+      +---+
+      |   |
+      O   |
+          |
+          |
+          |
+    =========
       `,
     `
-        +---+
-        |   |
-        O   |
-        |   |
-            |
-            |
-      =========
+      +---+
+      |   |
+      O   |
+      |   |
+          |
+          |
+    =========
       `,
     `
-        +---+
-        |   |
-        O   |
-       /|   |
-            |
-            |
-      =========
+      +---+
+      |   |
+      O   |
+     /|   |
+          |
+          |
+    =========
       `,
     `
-        +---+
-        |   |
-        O   |
-       /|\\\  |
-            |
-            |
-      =========
+      +---+
+      |   |
+      O   |
+     /|\\\  |
+          |
+          |
+    =========
       `,
     `
-        +---+
-        |   |
-        O   |
-       /|\\\  |
-       /    |
-            |
-      =========
+      +---+
+      |   |
+      O   |
+     /|\\\  |
+     /    |
+          |
+    =========
       `,
     `
-        +---+
-        |   |
-        O   |
-       /|\\\  |
-       / \\\  |
-            |
-      =========
+      +---+
+      |   |
+      O   |
+     /|\\\  |
+     / \\\  |
+          |
+    =========
       `,
   ],
   /**
