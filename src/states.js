51465cda39784a2b34361cfcb8d73122fb4e6043
Suvi.Sulonen <suvi.sulonen@tuni.fi>
2022-04-12 19:08:10 +0300

    Edit: (states.js) replace process.exit(0) with return 0 in both win and lose methods.

--- src/states.js
+++ src/states.js
@@ -1,7 +1,7 @@
 // @ts-check
 
 import hangman from "./hangman.js";
-// import chalk from "chalk";
+import chalk from "chalk";
 
 /**
  * Description to be added
@@ -16,22 +16,22 @@ const states = {
    * @param {Array} answerArray
    */
   win: (remainingLives, answerArray) => {
-    console.clear();
-    console.log(`  LIVES LEFT: ${remainingLives}`);
-    const lastStage = hangman.draw(remainingLives);
-    console.log(lastStage);
-    console.log("  ", answerArray.join(" "));
-    console.log(`\n    YOU WON!\n`);
-    process.exit(0);
-
-    // Using chalk
     // console.clear();
-    // console.log(chalk.dim(`  LIVES LEFT: ${remainingLives}`));
+    // console.log(`  LIVES LEFT: ${remainingLives}`);
     // const lastStage = hangman.draw(remainingLives);
-    // console.log(chalk.dim(lastStage));
-    // console.log(chalk.dim("  ", answerArray.join(" ")));
-    // console.log(chalk.green.bold(`\n    YOU WON!\n`));
+    // console.log(lastStage);
+    // console.log("  ", answerArray.join(" "));
+    // console.log(`\n    YOU WON!\n`);
     // process.exit(0);
+
+    //Using chalk
+    console.clear();
+    console.log(chalk.dim(`  LIVES LEFT: ${remainingLives}`));
+    const lastStage = hangman.draw(remainingLives);
+    console.log(chalk.dim(lastStage));
+    console.log(chalk.dim("  ", answerArray.join(" ")));
+    console.log(chalk.green.bold(`\n    YOU WON!\n`));
+    return 0;
   },
   /**
    * This will be shown if the player has lost the game.
@@ -40,20 +40,20 @@ const states = {
    * @param {String} chosenWord
    */
   lose: (remainingLives, chosenWord) => {
-    console.clear();
-    console.log(`  LIVES LEFT: ${remainingLives}`);
-    console.log(hangman.stages[6]);
-    console.log(`    GAME OVER`);
-    console.log(`\nTHE WORD WAS ${chosenWord}`);
-    process.exit(0);
+    // console.clear();
+    // console.log(`  LIVES LEFT: ${remainingLives}`);
+    // console.log(hangman.stages[6]);
+    // console.log(`    GAME OVER`);
+    // console.log(`\nTHE WORD WAS ${chosenWord}`);
+    // process.exit(0);
 
     // Using Chalk
-    //console.clear();
-    // console.log(chalk.dim(`  LIVES LEFT: ${remainingLives}`));
-    // console.log(chalk.dim(hangman.stages[6]));
-    // console.log(chalk.red.bold(`    GAME OVER`));
-    // console.log(chalk.dim(`\nTHE WORD WAS ${chosenWord}`));
-    //process.exit(0);
+    console.clear();
+    console.log(chalk.dim(`  LIVES LEFT: ${remainingLives}`));
+    console.log(chalk.dim(hangman.stages[6]));
+    console.log(chalk.red.bold(`    GAME OVER`));
+    console.log(chalk.dim(`\nTHE WORD WAS ${chosenWord}`));
+    return 0;
   },
 };
 
