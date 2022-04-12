32f7389cb2f2d0c8de27daf50e2eac2f5145ea78
Suvi.Sulonen <suvi.sulonen@tuni.fi>
2022-04-12 20:14:50 +0300

    Add: (player-input.js) to include license and copyright statement.

--- src/player-input.js
+++ src/player-input.js
@@ -3,6 +3,23 @@
 import readlineSync from "readline-sync";
 
 /**
+ * @license
+ * @copyright 2022
+ * @author Suvi Sulonen <suvi.sulonen@gmail.com>
+ *
+ * This file is part of Hangman Test Project.
+ *
+ * Hangman Test Project is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hangman Test Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Hangman Test Project. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * Contains everything related to the player. Includes the methods that handle getting the users input and reformatting it to fit the criterias given.
  * @module playerInput
  * @namespace playerInput
