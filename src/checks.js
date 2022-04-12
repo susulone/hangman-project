cd7647db8ab4f4febdb46179ec554baab3dc142a
Suvi.Sulonen <suvi.sulonen@tuni.fi>
2022-04-12 20:16:37 +0300

    Add: (checks.js) to include license and copyright statement.

--- src/checks.js
+++ src/checks.js
@@ -1,6 +1,23 @@
 // @ts-check
 
 /**
+ * @license
+ * @copyright 2022
+ * @author Suvi Sulonen <suvi.sulonen@gmail.com>
+ *
+ * This file is part of Hangman Test Project.
+ *
+ * Hangman Test Project is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hangman Test Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Hangman Test Project. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/**
  * This object holds all the functionality used to check if the player's guess was right and if all the letters have been guessed.
  * @module checks
  * @namespace checks
