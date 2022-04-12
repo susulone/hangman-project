ba2694704f24bca35bf9ac578ce037649b1a81ec
Suvi.Sulonen <suvi.sulonen@tuni.fi>
2022-04-12 20:12:08 +0300

    Add: (words.test.js) to include license and copyright statement.

--- tests/words.test.js
+++ tests/words.test.js
@@ -1,5 +1,22 @@
 import words from "../src/words";
 
+/**
+ * @license
+ * @copyright 2022
+ * @author Suvi Sulonen <suvi.sulonen@gmail.com>
+ *
+ * This file is part of Hangman Test Project.
+ *
+ * Hangman Test Project is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Hangman Test Project is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Hangman Test Project. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 describe("words", () => {
   it("should be an object", () => {
     expect(words).toBeObject();
